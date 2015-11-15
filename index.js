3b2570fcea8d8d37c9348d98b83ae6a0a6ae1ce6
Kawshal Chhetri <kawshalchhetri@gmail.com>
2015-11-15 21:42:02 +0530

    add

---
+++ index.js
@@ -0,0 +1,182 @@
+var data=
+{"domains": [
+    {
+        "Name": "BFS",
+        "Desc": "Banking & Financial Services",
+        "LOB": [
+            {
+                "Name": "Retail Banking",
+                "Function": [
+                    {
+                        "Name": "Deposits",
+                        "SubFunction": [
+                            "Accout balances",
+                            "Transaction details",
+                            "Transfers",
+                            "Bill payment"
+                        ]
+                    },
+                    {
+                        "Name": "Loans",
+                        "SubFunction": [
+                            "Accout Dues",
+                            "Loan Origination",
+                            "Loan Payment history",
+                            "Pay Loan installment",
+                            "Bill payment"
+                        ]
+                    },
+                    {
+                        "Name": "Investment Services",
+                        "SubFunction": [
+                            "Apply for Investments",
+                            "Redeem investments",
+                            "Net wealth calculator"
+                        ]
+                    },
+                    {
+                        "Name": "Insurance",
+                        "SubFunction": [
+                            "Insurance Originaion",
+                            "Underwriting",
+                            "Pay insurance premiums",
+                            "Claim submission",
+                            "Policy issuance"
+                        ]
+                    }
+                ]
+            },
+            {
+                "Name": "Mortagages & Retail lending",
+                "Function": [
+                    {
+                        "Name": "Mortgage loans",
+                        "SubFunction": [
+                            "Loan Origination",
+                            "Mortgage Rate Tracker",
+                            "EMI Calculator",
+                            "Home Appraisal",
+                            "Home Inspector App",
+                            "Loan Payment history",
+                            "Pay Loan installment",
+                            "Underwriting"
+                        ]
+                    },
+                    {
+                        "Name": "Retail Lending",
+                        "SubFunction": [
+                            "Personal loan Orgination",
+                            "Auto loan Orgination",
+                            "Educational loan Orgination",
+                            "Loan Payment history",
+                            "Pay Loan installment",
+                            "Credit Card Origination",
+                            "Pay Credit Card Dues"
+                        ]
+                    }
+                ]
+            },
+            {
+                "Name": "Corporate Banking",
+                "Function": [
+                    {
+                        "Name": "Cash Management",
+                        "SubFunction": [
+                            "Deposits",
+                            "Commercial Cards",
+                            "Receivables",
+                            "Payments",
+                            "Investments",
+                            "Forex services"
+                        ]
+                    },
+                    {
+                        "Name": "Trade Finance",
+                        "SubFunction": [
+                            "Letter of credit",
+                            "Documentary collections",
+                            "Trade services"
+                        ]
+                    },
+                    {
+                        "Name": "Loans",
+                        "SubFunction": [
+                            "Lines of Credit",
+                            "Term Loans",
+                            "Commercial Mortgage",
+                            "Asset Based Lending",
+                            "Small Business Loans"
+                        ]
+                    }
+                ]
+            },
+            {
+                "Name": " Wealth Management",
+                "Function": [
+                    {
+                        "Name": "Advisory Services",
+                        "SubFunction": [
+                            "Research & Analysis",
+                            "Analysis of the objectives & financials",
+                            "Portfolio Modeling",
+                            "Recommendation Preparation",
+                            "Portfolio Modeling",
+                            "Retirement Planning"
+                        ]
+                    },
+                    {
+                        "Name": "Investment Products",
+                        "SubFunction": [
+                            "Customer Acquisition",
+                            "KYC",
+                            "Account creation"
+                        ]
+                    },
+                    {
+                        "Name": "Investing",
+                        "SubFunction": [
+                            "Trade capture",
+                            "Trade reconciliation",
+                            "Financial tools"
+                        ]
+                    }
+                ]
+            }
+        ],
+        "UserTypes": [
+            "Contact Center (Bank)",
+            "Front Office (Bank)",
+            "Back Office (Bank)",
+            "Middle Office (Bank)",
+            "Supervision (Bank)",
+            "Bank Customer"
+        ]
+    },
+    {
+        "Name": "INS",
+        "Desc": "Insurance",
+        "UserTypes": [
+            "Insurance Call Center",
+            "Insurance Advisors",
+            "Insurance Customer"
+        ]
+    },
+    {
+        "Name": "TNH",
+        "Desc": "Travel and Hospitality",
+        "UserTypes": [
+            "Hospitality Customer",
+            "Back Office"
+        ]
+    },
+    {
+        "Name": "RTL",
+        "Desc": "Retail",
+        "UserTypes": [
+            "Online Customer",
+            "Retail Back Office"
+        ]
+    }
+]
+};
+
